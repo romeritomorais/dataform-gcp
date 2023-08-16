d9d8f51c772c4ecc200809a6905ac70a552e40f0
Romerito Morais <romeritomorais.sisx@gmail.com>
2023-08-16 05:10:41 +0000

    mm

--- includes/landing.js
+++ includes/landing.js
@@ -1,4 +1,4 @@
-const LND_DATABASE = 'caninana-labs';
+const LND_DATABASE = 'caninana-labs'; 
 const LND_DATASET = 'raw';
 const LND_CADF_CONVITE = 'external_table_csv';
 module.exports = {LND_DATABASE, LND_DATASET, LND_CADF_CONVITE}
